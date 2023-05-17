0f38b1c20341d7b6fb2c604fbf889b80789e4cd4
znichola <znichola@student.42lausanne.ch>
2023-05-18 00:01:29 +0200

    starting but go nowhere

--- includes/minirt_primitives.h
+++ includes/minirt_primitives.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt_primitives.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 22:41:17 by znichola          #+#    #+#             */
-/*   Updated: 2023/05/17 15:49:58 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/05/17 18:31:27 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ t_v3	get_pl_emmision(t_object *me, t_intersection *i);
 t_v3	get_pl_position(t_object *me);
 float	get_pl_poi(t_object *me, t_v3 ray, t_v3 source, t_intersection *i);
 t_v3	get_pl_poi_norm(t_object *obj, t_intersection *i);
-t_v2f	planar_map(t_plane *pl, t_v3 p);
+t_v2f	planar_map(t_plane *pl, t_intersection *i);
 
 /* cylinder.c */
 
--- srcs/primitives/plane.c
+++ srcs/primitives/plane.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   plane.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 22:47:01 by znichola          #+#    #+#             */
-/*   Updated: 2023/05/17 15:54:18 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/05/18 00:00:16 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ t_v3	get_pl_emmision(t_object *me, t_intersection *i)
 	pl = me->object.pl;
 	if (pl.checker)
 	{
-		if (get_pix_from_checkerboard(planar_map(&pl, i->poi)) == 0)
-			return ((t_v3){0,0,0});
+		if (get_pix_from_checkerboard(planar_map(&pl, i)) == 0)
+			return ((t_v3){1,1,1});
 	}
 	if (pl.texture.img != NULL)
 	{
-		return (get_pix_from_texture(&pl.texture, planar_map(&pl, i->poi)));
+		return (get_pix_from_texture(&pl.texture, planar_map(&pl, i)));
 	}
 	return (pl.colour);
 }
@@ -53,11 +53,34 @@ t_v3	get_pl_poi_norm(t_object *obj, t_intersection *i)
 	return (i->poi_normal);
 }
 
+#define DIV 50.0f
+
 /* map a 3d point on a plane to a 2d point on a map */
-t_v2f	planar_map(t_plane *pl, t_v3 p)
+t_v2f	planar_map(t_plane *pl, t_intersection *i)
 {
+	t_v3 new_x = v3_cross(pl->orientation, UP);
+	t_v3 new_z = v3_cross(new_x, pl->orientation);
+
+	t_v3 pos = v3_subtract(i->poi, pl->position);
+	float d = v3_mag(pos);
+	pos = v3_unitvec(pos);
+	t_v3 new = (t_v3)
+	{
+		v3_dot(new_x, pos),
+		v3_dot(pl->orientation, pos),
+		v3_dot(new_z, pos)
+	};
+	new = v3_multiply(new, d);
+	// float	v = ((new.x / DIV) - roundf(new.x / DIV));
+	// float	u = ((new.z / DIV) - roundf(new.z / DIV));
+	// float		u = fmodf(new.x, DIV);
+	// float		v = fmodf(new.z, DIV);
+	// printf("u:%.1f\n", u);
 	(void)pl;
-	float	v = ((p.x / 100) - roundf(p.x / 100));
-	float	u = ((p.z / 100) - roundf(p.z / 100));
+	(void)new;
+	float	u = ((i->poi.z / DIV) - roundf(i->poi.z / DIV));
+	float	v = ((i->poi.x / DIV) - roundf(i->poi.x / DIV));
+	// u = fabsf(u);
+	// v = fabsf(v);
 	return ((t_v2f){u, v});
 }
