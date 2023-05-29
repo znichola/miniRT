70780a3e26226ff44f56ed1a0051b27004213af9
Sophia Koulen <sophia.koulen@outlook.com>
2023-05-29 13:08:14 +0200

    repaired cylinder

--- srcs/maths/poi_cylinder2.c
+++ srcs/maths/poi_cylinder2.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:48:59 by skoulen           #+#    #+#             */
-/*   Updated: 2023/05/25 16:27:02 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/05/29 13:06:53 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ float	try_start_cap_sp(t_terms *t, t_cylinder *me, t_intersection *i)
 {
 	t_plane	p;
 
-	if ((v3_dot(me->orientation, t->ray)) >= FLT_EPSILON)
+	if ((v3_dot(me->orientation, t->ray)) > FLT_EPSILON)
 	{
 		p.position = me->position;
 		p.orientation = v3_multiply(me->orientation, -1);
-		poi_plane(&p, t->ray, t->source, i);
+		if (poi_plane(&p, t->ray, t->source, i) == FLT_MAX)
+			return (FLT_MAX);
 		if (v3_mag(v3_subtract(i->poi, p.position)) <= t->radius)
 		{
 			i->is_cap = 1;
@@ -40,12 +41,13 @@ float	try_end_cap_sp(t_terms *t, t_cylinder *me, t_intersection *i)
 {
 	t_plane	p;
 
-	if ((v3_dot(me->orientation, t->ray)) <= FLT_EPSILON)
+	if ((v3_dot(me->orientation, t->ray)) < -FLT_EPSILON)
 	{
 		p.position = v3_add(me->position,
 				v3_multiply(me->orientation, me->height));
 		p.orientation = me->orientation;
-		poi_plane(&p, t->ray, t->source, i);
+		if (poi_plane(&p, t->ray, t->source, i) == FLT_MAX)
+			return (FLT_MAX);
 		if (v3_mag(v3_subtract(i->poi, p.position)) <= t->radius)
 		{
 			i->is_cap = 1;
